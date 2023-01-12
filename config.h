2f2d099f168a4c2e3cd897e2e2e83ec31c8fc47d
thejc <threearapig@gmail.com>
2023-01-13 03:29:24 +0800

    the first stage

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -203,8 +203,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_u,           kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_e,           kscrolldown,    {.i =  1} },
+	{ MODKEY|ControlMask,   XK_u,           kscrollup,      {.i = -1} },
+	{ MODKEY|ControlMask,   XK_e,           kscrolldown,    {.i = -1} },
 };
 
 /*
