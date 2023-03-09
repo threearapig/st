b66d6519f904d2b0b1fa3fd939d350a540d4af8c
TheJC <threearapig@gmail.com>
2023-03-09 15:56:41 +0800

    change the value of alpha

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=22:antialias=true:autohint=true";
+//static char *font = "Source Code Pro:pixelsize=22:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=36:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -94,7 +95,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.82;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
